32c07af46564c301f118389d4a2d59f874f64edc
Monique Dali <68743809+mfdali@users.noreply.github.com>
2023-10-25 18:52:47 -0300

    Update code
    Add ILTA sample

--- get_aperture_Indiana.py
+++ get_aperture_Indiana.py
@@ -1,20 +1,18 @@
 import csv
 import matplotlib.pyplot as plt
-import csv
 import numpy as np
 from scipy.interpolate import griddata
 from scipy.spatial import distance
+
 '''
 Andesite
 files = 'surfaces_hang.csv' and 'surfaces_foot.csv'
 x.append(float(row[3]))
 y.append(float(row[1]))
 z.append(float(row[2]))
-
-
 '''
 
-def load_data(filename):
+def load_data(filename, x_col, y_col, z_col):
     x = []
     y = []
     z = []
@@ -23,9 +21,9 @@ def load_data(filename):
         csvreader = csv.reader(csvfile)
         next(csvreader)  # Skip the header row if present
         for row in csvreader:
-            x.append(float(row[1]))
-            y.append(float(row[0]))
-            z.append(float(row[2]))
+            x.append(float(row[x_col]))
+            y.append(float(row[y_col]))
+            z.append(float(row[z_col]))
             #print("%e,%e,%e\n"%(float(row[1]),float(row[0]),float(row[2])))
 
     return np.array(x), np.array(y), np.array(z)
@@ -47,22 +45,73 @@ def calculate_distance(Zup, Zdown):
     distance = np.sqrt((Zup - Zdown) ** 2)
     return distance
 
-a = 687
-b = 271
-output_folder = '/home/monique/fem-fenics/two-phase/fracture_results/'
+def il2b_params():
 
-upper_wall = output_folder + 'hanging_wall_IL2.csv'
-lower_wall = output_folder + 'foot_wall_IL2.csv'
+    a = 687
+    b = 271
+    input_folder = '/mnt/g/My Drive/Petrobras - Meio Fraturado/2023/fracture Indiana/'
+    filename_up = 'hanging_wall_IL2.csv'
+    filename_down = 'foot_wall_IL2.csv'
+    x_col = 1
+    y_col = 0
+    z_col = 2
+    x_steps = 4662
+    y_steps = 1821
+    return x_col, y_col, z_col, x_steps, y_steps, input_folder, filename_up, filename_down
 
-x = np.linspace(-0.0473847, 0.0509658, 4662)
-y = np.linspace(-0.0175, 0.018, 1821)
+def ilta_params():
+    input_folder = '/mnt/g/My Drive/Petrobras - Meio Fraturado/2023/fracture Indiana/geometry/ILTA/'
+    filename_up = 'ILTA_B_topology.csv'
+    filename_down = 'ILTA_A_topology.csv'
+    x_col = 1
+    y_col = 3
+    z_col = 2
+    x_steps = 1466
+    y_steps = 9596
+
+    return x_col, y_col, z_col, x_steps, y_steps, input_folder, filename_up, filename_down
+
+
+output_folder = '/mnt/g/My Drive/Petrobras - Meio Fraturado/2023/fracture Indiana/'
+
+sample = 'ILTA'
+
+if sample.lower() == 'ilta':
+    x_col, y_col, z_col, x_steps, y_steps, input_folder, filename_up, filename_down = ilta_params()
+if sample.lower() == 'il2b':    
+    x_col, y_col, z_col, x_steps, y_steps, input_folder, filename_up, filename_down = il2b_params()
+
+upper_wall = input_folder + filename_up
+lower_wall = input_folder + filename_down
+
+xup, yup, zup = load_data(upper_wall, x_col, y_col, z_col)
+xlow, ylow, zlow = load_data(lower_wall, x_col, y_col, z_col)
+
+xmin = min(np.min(xup),np.min(xlow))
+xmax = max(np.max(xup),np.max(xlow))
+ymin = min(np.min(yup),np.min(ylow))
+ymax = max(np.max(yup),np.max(ylow))
+
+num_points_in_line = len(yup)
+min_distances_up = []
+
+#for i in range(num_points_in_line):
+i = 20
+point1 = (xup[i], yup[i])  # Coordinates of the current point
+min_distance = float("inf")  # Initialize with a large value
+for j in range(num_points_in_line):
+    if i != j:  # Skip the current point
+        point2 = (xup[j], yup[j])  # Coordinates of another point
+        dist = distance.euclidean(point1, point2)
+        min_distance = min(min_distance, dist)
+min_distances_up.append(min_distance)
+
+x = np.linspace(xmin, xmax, x_steps)
+y = np.linspace(ymin, ymax, y_steps)
 X,Y = np.meshgrid(x,y)
-
-xup, yup, zup = load_data(upper_wall)
-xlow, ylow, zlow = load_data(lower_wall)
 
 Zup = matrixZ(X,Y,xup,yup,zup,output_folder,"hanging_IL2")
 Zdown = matrixZ(X,Y,xlow,ylow,zlow,output_folder,"foot_IL2")
 
 aperture = calculate_distance(Zup, Zdown)
-np.savetxt(output_folder + 'aperture_Indiana_IL2.csv', np.column_stack((X.ravel(), Y.ravel(), aperture.ravel())), fmt="%.8e", delimiter=",")  
+np.savetxt(output_folder + 'aperture_Indiana_' + sample + '.txt', np.column_stack((X.ravel(), Y.ravel(), aperture.ravel())), fmt="%.8e", delimiter=",")  
